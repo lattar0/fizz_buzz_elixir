e68c588966ec3b0012e6e7f3912bb6fdc4771b6b
João Francisco <42876444+lattaroo@users.noreply.github.com>
2022-09-27 23:55:06 -0300

    refactor

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,8 +1,8 @@
 defmodule FizzBuzz do
   def build (file_name) do
     file_name
-    |> File.read()
-    |> handle_file_read()
+    |> File.read
+    |> handle_file_read
   end
 
   defp handle_file_read({:ok, result}) do
@@ -18,8 +18,8 @@ defmodule FizzBuzz do
 
   defp convert_and_evaluate(elem) do
     elem
-    |> String.to_integer()
-    |> evaluate_numbers()
+    |> String.to_integer
+    |> evaluate_numbers
   end
 
   defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: "FizzBuzz"
